6040bd7d66fa87b5cce6f2559d51658070a66c7e
Tribhuwan <tribhuwanja@gmail.com>
2024-03-23 15:12:17 +0530

    github configuration added

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,3 @@ const App = () => {
 export default App;
 
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/tribanj/portfolio2.0.git
-// git push -u origin main
